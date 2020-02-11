22ff6f232232b2a59024c378b864f00359272cc1
Josep PINYOT <jpinyot@e2r8p1.42.fr>
2020-02-11 08:46:17 +0100

    linear regression not working

--- srcs/linearRegression.cpp
+++ srcs/linearRegression.cpp
@@ -6,7 +6,7 @@
 /*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 11:01:31 by jpinyot           #+#    #+#             */
-/*   Updated: 2020/02/10 12:23:35 by jpinyot          ###   ########.fr       */
+/*   Updated: 2020/02/11 08:45:21 by jpinyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,18 +43,36 @@ void	LinearRegression::writeData(string& line)
 	}
 }
 
+void	LinearRegression::linearRegression()
+{
+	int		size = price_.size();
+	/* while(1){ */
+	for(int j = 0; j < 1; j++){
+		/* cout << '&'; */
+		for (int i = 0; i < size; i++){
+			double estimatePrice = theta0_ + (theta1_ * mileage_[i]);
+			double error = estimatePrice - price_[i];
+			theta0_ = theta0_ - learningRate_ * error;
+			theta1_ = theta1_ - learningRate_ * error * mileage_[i];
+			cout << theta0_ << '\t' << theta1_ << '\t' << estimatePrice - price_[i] << '\n';
+		}
+	}
+}
+
 int main()
 {
 	LinearRegression lr;
+	lr.linearRegression();
 
 	vector<int> test = lr.mileage();
 	vector<int> test2 = lr.price();
+	double theta0 = lr.theta0();
+	double theta1 = lr.theta1();
 
-	for (int i = 0; test[i]; i++){
-		cout << test[i] << '\t';
-		cout << test2[i] << '\n';
-
-	}
-
+	cout << "\n\n" << theta0 << '\t' << theta1 << "\n\n" << theta0 + (theta1 * 22899) << "\n\n";
+	/* for (int i = 0; test[i]; i++){ */
+	/* 	cout << test[i] << '\t'; */
+	/* 	cout << test2[i] << '\n'; */
+	/* } */
 	return 0;
 }
--- srcs/linearRegression.h
+++ srcs/linearRegression.h
@@ -6,7 +6,7 @@
 /*   By: jpinyot <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/10 11:01:33 by jpinyot           #+#    #+#             */
-/*   Updated: 2020/02/10 12:23:35 by jpinyot          ###   ########.fr       */
+/*   Updated: 2020/02/11 08:45:22 by jpinyot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,16 @@
 
 #define FILE_NAME "files/data.csv"
 #define DELIMETER ','
+#define LEARING_RATE 0.2
 
 using namespace std;
 
 class LinearRegression
 {
 	private:
-		float			theta0_;
-		float			theta1_;
+		double			theta0_;
+		double			theta1_;
+		double			learningRate_;
 		vector<int>		mileage_;
 		vector<int>		price_;
 		string			dataInfo_;
@@ -35,11 +37,14 @@ class LinearRegression
 
 	public:
 		LinearRegression():
-			theta0_(0), theta1_(0), mileage_(0), price_(0), dataInfo_()
+			theta0_(0), theta1_(0), learningRate_(0.01), mileage_(0), price_(0), dataInfo_()
 			{
 				getData();
 			};
 		void		linearRegression();
+
+		double		theta0() {return theta0_;}
+		double		theta1() {return theta1_;}
 		vector<int>	mileage() {return mileage_;}
 		vector<int>	price() {return price_;}
 };
